0b53ea530b3b00f3a4abcf4085672429254c6f98
joseluisvaz <joseluisv1201@gmail.com>
2017-08-12 23:07:23 -0500

    first python script

---
+++ taxiscript.py
@@ -0,0 +1,13 @@
+# this is a script for the kaggle competition
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
+from datetime import timedelta
+import datetime as dt
+
+
+
+
+
